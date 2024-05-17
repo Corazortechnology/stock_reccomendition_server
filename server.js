5a7e4beeb4b7c2b5a9e03b5e7e49a76d4417f614
VAIBHAV ZADE <126239600+zade-vaibhav@users.noreply.github.com>
2024-05-17 18:39:59 +0530

    changing login responce

--- server.js
+++ server.js
@@ -14,10 +14,18 @@ mongodb();
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(path.resolve(__dirname,"public")))
+
 app.use(cors({
     origin: 'https://new-stock-reccomendition.vercel.app',
     credentials: true
   }));
+  app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(express.json())
 app.use(cookieParser())
 
