4179ee8faafc215d527b8a5a581cce0c4f8b7b78
VAIBHAV ZADE <126239600+zade-vaibhav@users.noreply.github.com>
2024-05-17 15:04:31 +0530

    changing cors prop

--- middlewears/verifyToken.js
+++ middlewears/verifyToken.js
@@ -1,7 +1,9 @@
 
 async function verify(req,res,next){
     const cookie =req.headers.cookies;
-    console.log(cookie)
+    const cookiess= req.headers.cookie
+    console.log(cookie ,cookiess)
+    next()
 }
 
 module.exports = {verify}
