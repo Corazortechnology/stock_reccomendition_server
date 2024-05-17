6019ab581747ae34e002a285ef35fe4ef7c8d1d7
VAIBHAV ZADE <126239600+zade-vaibhav@users.noreply.github.com>
2024-05-17 19:19:41 +0530

    changing login responce

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(path.resolve(__dirname,"public")))
 
 app.use(cors({
-    origin: 'https://new-stock-reccomendition.vercel.app',
+    origin:"https://new-stock-reccomendition.vercel.app/",
     credentials: true
   }));
 app.use(express.json())
